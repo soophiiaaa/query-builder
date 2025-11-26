1738f7dc3dd200006cc992a2034a2dac4f92fd95
Sophia Lacerda <sbuarquemagalhaes@gmail.com>
2025-11-26 17:52:51 -0300

    feat: start connection to database

--- src/Infrastructure/Config/database.php
+++
@@ -1,3 +0,0 @@
-<?php
-// aqui vão ficar os arquivos de configuração do banco de dados
-?>
--- src/Infrastructure/Database/Connection.php
+++ src/Infrastructure/Database/Connection.php
@@ -1,3 +1,32 @@
 <?php
-//aqui vai ser a conexão do banco
-?>
+
+namespace Sophia\QueryBuilder\Infrastructure\Database;
+
+use PDO;
+use PDOException;
+
+class Connection
+{
+    public static function define()
+    {
+        $host = $_ENV['DB_HOST'];
+        $user = $_ENV['DB_USER'];
+        $pass = $_ENV['DB_PASSWORD'];
+        $database = $_ENV['DB_NAME'];
+        $port = $_ENV['DB_PORT'];
+
+        // Variable that stores the database address to simplify the connection
+        $dsn = "pgsql:host={$host} port={$port} dbname={$database}";
+
+        try {
+            return new PDO(
+                $dsn,
+                $user,
+                $pass,
+                [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+            );
+        } catch (PDOException $e) {
+            throw new \Exception("Error connecting to database: {$e->getMessage()}");
+        }
+    }
+}
