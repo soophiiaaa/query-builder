e9085ea2cb2c8491add34c7e442326d7a910b5f4
Sophia Lacerda <sbuarquemagalhaes@gmail.com>
2025-12-03 23:03:00 -0300

    test: add tests for Criteria class

--- tests/CriteriaTest.php
+++ tests/CriteriaTest.php
@@ -9,6 +9,11 @@ use Sophia\QueryBuilder\Core\Criteria;
 $criteria1 = new Criteria;
 $criteria2 = new Criteria;
 $criteria3 = new Criteria;
+$criteria4 = new Criteria;
+$criteria5 = new Criteria;
+$criteria6 = new Criteria;
+$criteria7 = new Criteria;
+$criteria8 = new Criteria;
 
 $criteria1->add(new Filter(
     'idade',
@@ -56,6 +61,77 @@ $criteria3->add(new Filter(
     Expression::OR_OPERATOR
 );
 
+$criteria4->add(new Filter(
+    'telefone',
+    'IS NOT',
+    NULL
+    ),
+);
+
+$criteria4->add(new Filter(
+    'sexo',
+    '=',
+    'F'
+    )
+);
+
+$criteria5->add(new Filter(
+    'UF',
+    'IN',
+    ['RS', 'SC', 'PR']
+    )
+);
+
+$criteria5->add(new Filter(
+    'UF',
+    'IN NOT',
+    ['AC', 'PI']
+    )
+);
+
+$criteria6->add(new Filter(
+    'sexo',
+    '=',
+    'F'
+    )
+);
+
+$criteria6->add(new Filter(
+    'idade',
+    '>',
+    18
+    )
+);
+
+$criteria7->add(new Filter(
+    'sexo',
+    '=',
+    'M'
+    )
+);
+
+$criteria7->add(new Filter(
+    'idade',
+    '<',
+    16
+    )
+);
+
+$criteria8->add(
+    $criteria6,
+    Expression::OR_OPERATOR
+);
+
+$criteria8->add(
+    $criteria7,
+    Expression::OR_OPERATOR
+);
+
 echo $criteria1->dump() . "\n";
 echo $criteria2->dump() . "\n";
 echo $criteria3->dump() . "\n";
+echo $criteria4->dump() . "\n";
+echo $criteria5->dump() . "\n";
+echo $criteria6->dump() . "\n";
+echo $criteria7->dump() . "\n";
+echo $criteria8->dump() . "\n";
